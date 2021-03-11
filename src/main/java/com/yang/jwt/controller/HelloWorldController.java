5632a660d6d433b588ef0f147e52da5e3e14b8c9
P042360 <P042360@host.docker.internal>
2021-03-11 14:57:49 +0900

    commit

--- src/main/java/com/yang/jwt/controller/HelloWorldController.java
+++ src/main/java/com/yang/jwt/controller/HelloWorldController.java
@@ -1,10 +1,7 @@
 package com.yang.jwt.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.yang.jwt.config.JwtTokenUtil;
 
 @RestController
 public class HelloWorldController {
