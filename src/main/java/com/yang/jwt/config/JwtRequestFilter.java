8164a3c26ae9eea96544fec8110d59863e6ae9ad
P042360 <P042360@host.docker.internal>
2021-03-11 14:27:47 +0900

    commit

--- src/main/java/com/yang/jwt/config/JwtRequestFilter.java
+++ src/main/java/com/yang/jwt/config/JwtRequestFilter.java
@@ -1,10 +1,12 @@
 package com.yang.jwt.config;
 
 import java.io.IOException;
+
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -13,8 +15,6 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
-import com.yang.jwt.model.JwtRequest;
-import com.yang.jwt.model.UserInfo;
 import com.yang.jwt.service.JwtUserDetailsService;
 
 import io.jsonwebtoken.ExpiredJwtException;
